6b97f4fe0533a53af904f678a0c65cb0711fc73f
may <mayrautopia@gmail.com>
2022-09-12 16:37:59 -0300

    finished

---
+++ src/helpers/getUrl.js
@@ -0,0 +1,10 @@
+const {request} = require('express');
+
+const getUrl = (request) => {
+    return request.protocol + "://" + request.get('host') + request.originalUrl;
+}
+
+
+module.exports = {
+    getUrl
+}
