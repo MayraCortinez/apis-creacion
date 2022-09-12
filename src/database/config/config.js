a7a5bb4b192b7c6db9d2b6659d7b2e358718818d
MayraCortinez <99737864+MayraCortinez@users.noreply.github.com>
2022-09-12 17:05:46 -0300

    Update config.js

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 module.exports = {
   "development": {
     "username": "root",
-    "password": process.env.DB_PASSWORD,
+    "password": "root",
     "database": "movies_db",
     "host": "127.0.0.1",
     "dialect": "mysql"
